49fcda88fdf7a50f5a67418471e0b19ccf82fea1
cunamy <voropaeven@yandex.ru>
2019-09-26 00:00:39 +0500

    Add function RecursiveSum

--- Library/LibraryAlgorithms.cs
+++ Library/LibraryAlgorithms.cs
@@ -18,7 +18,39 @@ namespace library
                 else low = mid+1;
             }
             return -1;
+        }
+        public int FindSmallest<T>(List<T> list) where T : IComparable
+        {
+            T min = list[0];
+            int minIndex = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(min) < 0)
+                {
+                    minIndex = i;
+                    min = list[i];
+                }
+            }
+            return minIndex; 
+        }
 
+        public List<T> SelectionSort<T>(List<T> list)where T : IComparable
+        {
+            var newList = new List<T>();
+            while(list.Count != 0)
+            {
+                var smallest = FindSmallest(list);
+                newList.Add(list[smallest]);
+                list.RemoveAt(smallest);
+            }
+            return newList;
+        }
+
+        public double   RecursiveSum(List<double> list) 
+        {
+            if (list.Count == 0) return 0;
+            if (list.Count == 1) return list[0];
+            return list[0] + RecursiveSum(list.GetRange(1,list.Count - 1 ));
         }
     }
 }
--- Tests/TestAlgorithms.cs
+++ Tests/TestAlgorithms.cs
@@ -18,5 +18,32 @@ namespace tests
             Assert.Equal(result, 1);
             
         }
+
+        [Fact]
+        public void FindSmallestTest()
+        {
+            var library = new LibraryAlgorithms();
+            var list = new List<int>{6,3,5,1,9};
+            var result = library.FindSmallest<int>(list);
+            Assert.Equal(result, 3);            
+        }
+
+        [Fact]
+        public void SelectionSortTest()
+        {
+            var library = new LibraryAlgorithms();
+            var list = new List<int>{5,3,6,2,10};
+            var result = library.SelectionSort<int>(list);
+            Assert.Equal(new int[]{2,3,5,6,10},result.ToArray());            
+        }
+
+         [Fact]
+        public void RecursiveSumTest()
+        {
+            var library = new LibraryAlgorithms();
+            var list = new List<double>{5,3,6,2,10};
+            var result = library.RecursiveSum(list);
+            Assert.Equal(26,result);            
+        }
     }
 }
